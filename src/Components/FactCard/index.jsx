d15d6cf043f3379a1d0d5e3bc88ab6cc434f112c
Hyde Stefan <79136747+DrHyde01@users.noreply.github.com>
2022-01-28 16:33:31 +0100

    Add fact card

--- src/Components/FactCard/index.jsx
+++ src/Components/FactCard/index.jsx
@@ -1,7 +1,17 @@
+import { RefreshIcon } from '@heroicons/react/solid'
+
 function FactCard({ fact }) {
   return (
-    <div className="p-20 bg-white rounded-xl shadow-xl">
-      <p className="text-xl font-extralight">{fact}</p>
+    <div className="relative flex flex-col w-full">
+      <div className="flex justify-center items-center p-6 w-1/4 bg-violet-600 rounded-3xl shadow-md -rotate-12 -translate-x-14 translate-y-10">
+        <p className="font-semibold text-xl text-white">Do you know ?</p>
+      </div>
+      <div className="flex p-20 justify-center bg-white rounded-xl shadow-xl">
+        <p className="text-xl font-extralight">{fact}</p>
+      </div>
+      <button className="absolute -right-5 -bottom-5 flex justify-center items-center p-3 h-14 w-14 bg-pink-600 rounded-full shadow-md  ease-in-out duration-500 hover:bg-pink-500">
+        <RefreshIcon className=" text-white ease-in-out duration-500 hover:rotate-90" />
+      </button>
     </div>
   )
 }
